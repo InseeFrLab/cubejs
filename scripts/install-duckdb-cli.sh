e3572c3ad65d90c06d48968f5fae083628367805
Fanny Salvan <63729260+FaSalv@users.noreply.github.com>
2026-06-25 14:48:25 +0200

    build: downgrade duckdb to 1.4.1 to match cubejs duckdb version

--- scripts/install-duckdb-cli.sh
+++ scripts/install-duckdb-cli.sh
@@ -17,7 +17,7 @@ case $ARCH in
 esac
 
 echo "Downloading $FILENAME..."
-wget -O "$FILENAME" "https://github.com/duckdb/duckdb/releases/download/v1.4.4/$FILENAME"
+wget -O "$FILENAME" "https://github.com/duckdb/duckdb/releases/download/v1.4.1/$FILENAME"
 
 
 echo "Trying to unzip..."
